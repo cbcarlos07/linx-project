bbc5e28c99c5af186fb14592dbd5ec1603dfbbe2
Carlos <cbcarlos07@gmail.com>
2020-02-19 22:34:28 -0400

    -

--- api-product/src/service/mysql/index.ts
+++ api-product/src/service/mysql/index.ts
@@ -15,7 +15,8 @@ export class Config {
             database: process.env.DB_DATABASE,
             user: process.env.DB_USER,
             password: process.env.DB_PWD,
-            host: process.env.DB_HOST
+            host: process.env.DB_HOST,
+            port: +process.env.DB_PORT
         })
         return this.conn
     }
